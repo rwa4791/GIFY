c1861d6db3e68729ef4eb135b1b98b77d0f20487
Bobby Angus <Bobby Angus>
2017-09-26 22:16:40 -0400

    homework

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+var showTitle = ['Game of Thrones', 'Sherlock bbc', 'Mr. Robot', 'The Walking Dead', 'Daredevil', 'Unbreakable Kimmy Schmidt', 'Silicon Valley', 'The Flash', 'Supergirl', 'Louie', 'iZombie', 'Homeland', 'Arrow', 'Veep', 'Parks and Recreation', 'Bobs Burger'];
+var currentGif; var pausedGif; var animatedGif; var stillGif;
+
+//creates buttons
+function createButtons(){
+	$('#TVButtons').empty();
+	for(var i = 0; i < showTitle.length; i++){
+		var showBtn = $('<button>').text(showTitle[i]).addClass('showBtn').attr({'data-name': showTitle[i]});
+		$('#TVButtons').append(showBtn);
+	}
+
+	//displays gifs on click
+	$('.showBtn').on('click', function(){
+		$('.display').empty();
+
+		var thisShow = $(this).data('name');
+		var giphyURL = "http://api.giphy.com/v1/gifs/search?q=tv+show+" + thisShow + "&limit=10&api_key=dc6zaTOxFJmzC";
+		$.ajax({url: giphyURL, method: 'GET'}).done(function(giphy){
+			console.log(giphy);
+			currentGif = giphy.data;
+			$.each(currentGif, function(index,value){
+				animatedGif= value.images.original.url;
+				pausedGif = value.images.original_still.url;
+				var thisRating = value.rating;
+				//gives blank ratings 'unrated' text
+				if(thisRating == ''){
+					thisRating = 'unrated';
+				}
+				var rating = $('<h5>').html('Rated: '+thisRating).addClass('ratingStyle');
+				stillGif= $('<img>').attr('data-animated', animatedGif).attr('data-paused', pausedGif).attr('src', pausedGif).addClass('playOnHover');
+				var fullGifDisplay = $('<button>').append(rating, stillGif);
+				$('.display').append(fullGifDisplay);
+			});
+		});
+	});
+}
+
+//animates and pauses gif on hover
+$(document).on('mouseover','.playOnHover', function(){
+ 	   	$(this).attr('src', $(this).data('animated'));
+ });
+ $(document).on('mouseleave','.playOnHover', function(){
+ 	   	$(this).attr('src', $(this).data('paused'));
+ });
+
+//sets a button from input
+$('#addShow').on('click', function(){
+	var newShow = $('#newShowInput').val().trim();
+	showTitle.push(newShow);
+	createButtons();
+	return false;
+});
+
+createButtons();
